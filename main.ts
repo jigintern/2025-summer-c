e146c30a685cc5b60e4e7a95fe4b3732ba75bc81
SHO800 <86473181+SHO800@users.noreply.github.com>
2025-08-24 21:44:05 +0900

    [update] フロントから.tsファイルを要求された際に.jsにトランスパイルして返す処理を追加
    # Conflicts:
    #	main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,37 @@
-import { serveDir } from 'jsr:@std/http/file-server';
+import {serveDir} from 'jsr:@std/http/file-server';
+import console from 'node:console';
+import {join} from 'jsr:@std/path';
+import {bundle} from 'jsr:@deno/emit';
+import denoConfig from './deno.json' with {type: 'json'};
+
+const publicRoot = join(Deno.cwd(), 'public');
 
 Deno.serve(async (req) => {
 	const pathname = new URL(req.url).pathname;
 	console.log(pathname);
 
+	// .tsをトランスパイルするブロック
+	if (pathname.endsWith('.ts')) { // URLの末尾が.tsなら
+		const tsPath = join(publicRoot, pathname); // public/<ファイルパス>を得る
+		try {
+			// トランスパイルして結果のコードを得る
+			const { code } = await bundle(tsPath, {
+				importMap: denoConfig,
+			});
+
+			// トランスパイルした結果のコードを返す
+			return new Response(code, {
+				headers: {
+					'Content-Type': 'application/javascript; charset=utf-8',
+					'Cache-Control': 'public, max-age=31536000, immutable',
+				},
+			});
+		} catch (error) { // トランスパイル時にエラーが発生した際
+			console.error('Error bundling TypeScript file:', error);
+			return new Response('Internal Server Error', { status: 500 });
+		}
+	}
+    
 	if (req.method === 'GET' && pathname === '/welcome-message') {
 		return new Response('jigインターンへようこそ！');
 	}
