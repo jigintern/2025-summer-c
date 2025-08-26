8b82f2318d19f13dcb4acf8f777ab62a4dcd091a
Tyara-173 <tyara1031@gmail.com>
2025-08-26 11:19:36 +0900

    [update] find()

--- backend/database.ts
+++ backend/database.ts
@@ -27,17 +27,17 @@ export async function findById(kv: Deno.Kv, id: string){
 
 export async function find(kv: Deno.Kv, year: number, x: number, y: number, x2: number, y2: number){
     const retID = await findByDecade(kv, year, year+10);
-    let ans : JSON  = []
-    for(const i of retID){
-        const data = JSON.parse(JSON.stringify(await findById(kv, i.value)))["value"];
-        const con = data["coordinate"]
-        const dataX = con["x"]
-        const dataY = con["y"]
+    let ans : any[]  = []
+    for await (const i of retID){
+        const dat = await findById(kv, i.value);
+        const data : ItemData = dat["value"] as ItemData;
+        const con = data["coordinate"];
+        const dataX = con["x"];
+        const dataY = con["y"];
         if(x <= dataX && dataX <= x2 && y <= dataY && dataY <= y2){
             ans.push(JSON.stringify(data));
-            // const test : ItemData = {...data};
         }
     }
-    console.log(x, y, x2, y2)
+    console.log(x, y, x2, y2);
     return ans;
 }
