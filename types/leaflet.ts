20f9b7b56fe8e1858774a75414c668eba3ec3035
SHO800 <86473181+SHO800@users.noreply.github.com>
2025-08-28 11:40:41 +0900

    [update] 仮デザイン

--- types/leaflet.ts
+++ types/leaflet.ts
@@ -12,9 +12,9 @@ export interface LeafletLayer {
 
 // L.LayerGroupの最小限の定義
 export interface LeafletLayerGroup extends LeafletLayer {
-  clearLayers(): this;
-  addLayer(layer: LeafletLayer): this;
-  removeLayer(layer: LeafletLayer): this;
+	clearLayers(): this;
+	addLayer(layer: LeafletLayer): this;
+	removeLayer(layer: LeafletLayer): this;
 }
 
 // L.Rectangleの最小限の定義
@@ -22,66 +22,78 @@ export interface LeafletRectangle extends LeafletLayer {}
 
 /** L.Mapの最小限の定義 */
 export interface LeafletMap {
-  getBounds(): LeafletLatLngBounds;
-  getZoom(): number;
-  hasLayer(layer: LeafletLayer): boolean;
-  removeLayer(layer: LeafletLayer): this;
-  addLayer(layer: LeafletLayer | LeafletLayerGroup): this;
-  setView(center: [number, number], zoom: number): this;
-  on(type: string, fn: (event: LeafletEvent) => void): this;
-  addControl(control: LeafletControl): this;
+	getBounds(): LeafletLatLngBounds;
+	getZoom(): number;
+	hasLayer(layer: LeafletLayer): boolean;
+	removeLayer(layer: LeafletLayer): this;
+	addLayer(layer: LeafletLayer | LeafletLayerGroup): this;
+	setView(center: [number, number], zoom: number, option?: object): this;
+	on(type: string, fn: (event: LeafletEvent) => void): this;
+	addControl(control: LeafletControl): this;
 
-  // map-initializer.ts で追加されたカスタムプロパティ
-  markerLayer: LeafletLayerGroup;
-  drawnItems: LeafletLayerGroup; // 追加
-  addInfoBox: (data: PostSubmission) => LeafletLayer;
+	// map-initializer.ts で追加されたカスタムプロパティ
+	markerLayer: LeafletLayerGroup;
+	drawnItems: LeafletLayerGroup; // 追加
+	addInfoBox: (data: PostSubmission) => LeafletRectangle;
+
+	fitBounds(bounds: LeafletLatLngBounds): void;
+
+    getBoundsZoom(bounds: LeafletLatLngBounds, b: boolean): number;
+
+    project(nw: LeafletLatLng, zoom: number): any;
+
+    getSize(): any;
+
+    unproject(centerPixel: any, zoom: number): any;
 }
 
 /** L.LatLngの最小限の定義 */
 export interface LeafletLatLng {
-  lat: number;
-  lng: number;
+	lat: number;
+	lng: number;
 }
 
 /** L.LatLngBoundsの最小限の定義 */
 export interface LeafletLatLngBounds {
-  getSouthWest(): LeafletLatLng;
-  getNorthEast(): LeafletLatLng;
+	getSouthWest(): LeafletLatLng;
+	getNorthEast(): LeafletLatLng;
+
+    getNorthWest(): LeafletLatLng;
 }
 
 // 一般的なイベント
 export interface LeafletEvent {
-    // Leafletイベントの基本プロパティ
+	// Leafletイベントの基本プロパティ
 }
 
 // 描画作成時の特定のイベント
 export interface LeafletDrawEvent extends LeafletEvent {
-  layer: LeafletLayer;
-  layerType: string;
+	layer: LeafletLayer;
+	layerType: string;
 }
 
 // Leafletコントロールの最小限の定義
 export interface LeafletControl {
-    // コントロールの基本
+	// コントロールの基本
 }
 
 // 描画ハンドラのインターフェース
 export interface LeafletDrawer {
-    enable(): void;
+	enable(): void;
 }
 
 // L.Drawのツールを定義
 export interface LeafletDrawTools {
-    Rectangle: new (map: LeafletMap, options?: object) => LeafletDrawer;
-    Polygon: new (map: LeafletMap, options?: object) => LeafletDrawer;
-    Circle: new (map: LeafletMap, options?: object) => LeafletDrawer;
+	Rectangle: new (map: LeafletMap, options?: object) => LeafletDrawer;
+	Polygon: new (map: LeafletMap, options?: object) => LeafletDrawer;
+	Circle: new (map: LeafletMap, options?: object) => LeafletDrawer;
 }
 
 // L.Drawのイベントとツールを結合
 export type LeafletDraw = LeafletDrawTools & {
-    Event: {
-        CREATED: string;
-    };
+	Event: {
+		CREATED: string;
+	};
 };
 
 // グローバルなLオブジェクトの最小限の定義
